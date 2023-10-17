ebb7219d7b2d53f56614ec2800b2700819f0f298
Ramune6110 <Arduino6110@gmail.com>
2023-10-17 16:17:39 +0900

    [fix]06

--- 01_Arrays&Hashing/06_Product of Array Except Self.cpp
+++ 01_Arrays&Hashing/06_Product of Array Except Self.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
+        int n = nums.size();
+        vector<int> result(n, 1);
+
+        int prefix = 1;
+        for (int i = 0; i < n; i++) {
+            result[i] = prefix;
+            prefix = prefix * nums[i];
+        }
+
+        int posfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            result[i] = result[i] * posfix;
+            posfix = posfix * nums[i];
+        }
+
+        return result;
     }
 };
